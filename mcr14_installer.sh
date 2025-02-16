2047038b252e20bb17a981c787b0a77774ead4eb
memesmith0 <meme.smith.wizard@gmail.com>
2025-02-16 02:14:59 -0800

    Create mcr14_installer.sh
    created installation script

---
+++ mcr14_installer.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/sh
+#Copyright (C) 2025 by john morris beck <john.morris.beck@hotmail.com>
+#
+#Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted.
+#
+#THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL
+#
+#IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER
+#
+#RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+#
+#THIS SOFTWARE.
+#https://github.com/memesmith0/mcr14
+#to run this script type: sh mcr14_installer.sh
+mkdir mcr14;
+cd mcr14;
+wget https://github.com/memesmith0/mcr14/blob/main/mcr14;
+wget https://github.com/memesmith0/mcr14/blob/main/mcr14sh;
+chmod +x mcr14;
+chmod +x mcr14sh;
+echo "you may now run mcr14 by typing: sh mcr14sh";
